d6e366efb97a0a55b90e2b1abab02f3b19d2724a
gabriel-melo284 <ginacioam@gmail.com>
2025-11-05 12:07:01 -0300

    Update App.jsx

--- meu-cardapio/src/App.jsx
+++ meu-cardapio/src/App.jsx
@@ -242,8 +242,6 @@ export default function App(){
         </div>
       </div>
 
-      </div>
-
       {/* Top bar (lupa à esquerda) */}
       <div className="sticky top-0 z-30 bg-white border-b">
         <div className="max-w-7xl mx-auto px-4 py-3">
